201dc769dca355b1167c68e17992b53200f3863d
jimlauj <164953973+jimlauj@users.noreply.github.com>
2024-09-10 19:29:55 -0500

    Updated field names

--- app.py
+++ app.py
@@ -13,15 +13,14 @@ st.title('Titanic Survival Prediction')
 
 # Input form for passenger details
 st.write('Enter Passenger Details:')
-Pclass = st.selectbox('Pclass', [1, 2, 3])
+Pclass = st.selectbox('Passenger Class', [1, 2, 3])
 Sex = st.selectbox('Sex', ['male', 'female'])
 Age = st.slider('Age', 0, 100, 30)
-Siblings_and_Spouses = st.number_input('SibSp', min_value=0, max_value=10, value=0)
-Number_of_Parents_or_Children = st.number_input('Parch', min_value=0, max_value=10, value=0)
+Siblings_and_Spouses = st.number_input('Siblings and Spouses', min_value=0, max_value=10, value=0)
+Number_of_Parents_or_Children = st.number_input('Number of Parents or Children', min_value=0, max_value=10, value=0)
 Fare = st.number_input('Fare', min_value=0.0, value=50.0)
-#Embarked_Q = 1 if st.selectbox('Embarked_Q', ['Yes', 'No']) == 'Yes' else 0
-#Embarked_S = 1 if st.selectbox('Embarked_S', ['Yes', 'No']) == 'Yes' else 0
-
+#Siblings_and_Spouses = SibSp
+#Number_of_Parents_or_Children = Parch
 # Make prediction based on input values
 
 scaler.mean_ = [31.58649832, 32.20420797]
@@ -32,10 +31,11 @@ input_data = [[Pclass, 1 if Sex == 'male' else 0, scaled_data[0][0], Siblings_an
 prediction = model.predict(numpy.array(input_data))
 
 if st.button('Predict'):
-    if prediction*100 >= 70:
+    #added a threshhold of 75% due to prediction either having most additions as survied or not survived.
+    if prediction*100 >= 75:
         st.write('The passenger is likely to survive.')
 
     else:
         st.write('The passenger is unlikely to survive.')
     st.write(f'The passenger has a {prediction*100} chance of survival.')
-    st.write(f'{input_data}')
+    #st.write(f'{input_data}')
